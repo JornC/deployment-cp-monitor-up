48f252dbf6f37a40d4d3c3d15a7d66cba3a2d197
Jorn Cruijsen <jorncruijsen@gmail.com>
2018-03-06 18:49:24 +0100

    Integrated the mock service
    Glorious...

---
+++ services/MONITORUP_WUI/MockHost_bu.java
@@ -0,0 +1,10 @@
+package nl.overheid.aerius.wui;
+
+import nl.overheid.aerius.wui.ServiceEndPoint;
+
+public class MockHost extends ServiceEndPoint {
+  @Override
+  public String get() {
+    return "http://{{cp.pr.id}}.mock.{{cp.deployment.host}}";
+  }
+}
--- services/MONITORUP_WUI/install.sh
+++ services/MONITORUP_WUI/install.sh
@@ -1,1 +1,1 @@
-docker build . -t monitorup-wui:{{service.local.hash}} --build-arg gitHash={{cp.pr.hash}} --build-arg oAuthToken={{cp.github.oath.token}}
+docker build . -t monitorup-wui:{{service.local.hash}} --build-arg gitHash={{cp.pr.hash}} --build-arg prId={{cp.pr.id}} --build-arg oAuthToken={{cp.github.oath.token}}
