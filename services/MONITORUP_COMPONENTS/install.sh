0d46825c46175e2200051785d9de0a6048bf0486
Jorn Cruijsen <jorncruijsen@gmail.com>
2018-03-06 14:17:31 +0100

    fixed things

--- services/MONITORUP_COMPONENTS/install.sh
+++ services/MONITORUP_COMPONENTS/install.sh
@@ -1,1 +1,1 @@
-docker build . -t monitorup-wui:{{service.local.hash}} --build-arg gitHash={{cp.git.hash}} --build-arg oAuthToken={{cp.github.oath.token}}
+docker build . -t monitorup-components:{{service.local.hash}} --build-arg oAuthToken={{cp.github.oath.token}}
--- services/MONITORUP_WUI/install.sh
+++ services/MONITORUP_WUI/install.sh
@@ -1,1 +1,1 @@
-docker build . -build-arg gitHash={{cp.pr.hash}} -build-argn oAuthToken={{cp.github.oath.token}}
+docker build . -t monitorup-wui:{{service.local.hash}} --build-arg gitHash={{cp.pr.hash}} --build-arg oAuthToken={{cp.github.oath.token}}
