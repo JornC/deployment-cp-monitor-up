96ca4585e28735b892815aace4e109441b561873
Jorn C <jorncruijsen@gmail.com>
2018-06-29 11:59:54 +0000

    Updated host

--- services/MONITORUP_WUI/MockHost.java
+++ services/MONITORUP_WUI/MockHost.java
@@ -5,6 +5,6 @@ import nl.overheid.aerius.wui.ServiceEndPoint;
 public class MockHost extends ServiceEndPoint {
   @Override
   public String get() {
-    return "http://{{cp.pr.id}}.mock.{{cp.deployment.host}}";
+    return "http://{{cp.pr.id}}.mock.{{cp.deployment.host}}/";
   }
 }
